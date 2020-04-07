2f0b6e5a56bbf541db7c9e7df7d1fbfabd8d1d35
Bota Gergely <botagergo@gmail.com>
2020-04-07 17:35:47 +0200

    added some logic for changing the current objective

--- javaagents/src/main/java/massim/javaagents/agents/MagellanAgent.java
+++ javaagents/src/main/java/massim/javaagents/agents/MagellanAgent.java
@@ -1,15 +1,14 @@
 package massim.javaagents.agents;
 
-import massim.javaagents.AgentMap;
-
-import eis.iilang.*;
-import massim.javaagents.MailService;
-
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import eis.iilang.*;
+import massim.javaagents.AgentMap;
+import massim.javaagents.Coord;
+import massim.javaagents.MailService;
 
 public class MagellanAgent extends Agent {
 
@@ -22,6 +21,8 @@ public class MagellanAgent extends Agent {
     private AgentMap map;
     private Objective currentObjective;
     private Direction prevDirection;
+    private Percept currentTask;
+    private Coord targetBlock;
 
     enum Objective {
         Discovery, GetMaterial, DropMaterial
@@ -57,12 +58,73 @@ public class MagellanAgent extends Agent {
             if (param instanceof Identifier)
                 say("Step " + ((Identifier) param).getValue());
         });
+
+        List<Percept> goalList = percepts.stream().filter(p -> p.getName().equals("goal")).collect(Collectors.toList());
+
+        updateCurrentObjective(percepts);
+
+        switch(currentObjective)
+        {
+            case Discovery:
+                return stepDiscovery(percepts);
+            case GetMaterial:
+                return stepGetMaterial();
+            case DropMaterial:
+                return stepDropMaterial();
+        }
+
+        return new Action("move", new Identifier("n"));
+    }
+
+    private void updateCurrentObjective(List<Percept> percepts)
+    {
+        List<Percept> taskList = percepts.stream().filter(p -> p.getName().equals("task")).collect(Collectors.toList());
+
+        if (taskList.isEmpty())
+        {
+            currentTask = null;
+            currentObjective = Objective.Discovery;
+        }
+        else if (!taskList.contains(currentTask))
+        {
+            System.out.println("found a new task!");
+            currentTask = taskList.get(0);
+        }
+
+        if (currentObjective == Objective.GetMaterial && (targetBlock = getNextBlock()) == null)
+            currentObjective = Objective.Discovery;
+
+        if (hasBlockForTask(currentTask))
+            currentObjective = Objective.DropMaterial;
+    }
+
+    private Coord getNextBlock()
+    {
+        return null;
+    }
+
+    private Boolean hasBlockForTask(Percept task)
+    {
+        return false;
+    }
+
+    private Action stepDiscovery(List<Percept> percepts)
+    {
         List<Percept> obstacleList = percepts.stream().filter(p -> p.getName().equals("obstacle"))
                 .collect(Collectors.toList());
         List<Percept> thingList = percepts.stream().filter(p -> p.getName().equals("thing"))
                 .collect(Collectors.toList());
-        List<Percept> goalList = percepts.stream().filter(p -> p.getName().equals("goal")).collect(Collectors.toList());
         return new Action("move", this.getDirection(obstacleList, thingList));
+    }
+
+    private Action stepGetMaterial()
+    {
+        return null;
+    }
+
+    private Action stepDropMaterial()
+    {
+        return null;
     }
 
     public void updateMap() {
